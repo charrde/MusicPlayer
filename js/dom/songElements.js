df73db37b358ee17d3f28f0dcb90ed1c80f91cc4
charrde <charrrde@gmail.com>
2024-06-21 02:11:07 -0600

    Simplified, media center, colors and styles.
    Added styling to a lot of the page including colors, fonts, sizing and positioning.
    Added new media center that appears when you press play on a song.
    Styled the play button more.
    Now displaying title and artist.
    Simplified the logic by making one audio player instead of each song having an audio player allowing easier access and more QOL benefits.
    Added more songs.
    Made sure the random rotation of songs doesn't put the same song multiple times.
    Volume has been temporarily hidden.

--- js/dom/songElements.js
+++ js/dom/songElements.js
@@ -1,107 +1,48 @@
-let currentAudio = null;
-let volumeLevel = 0.6;
+import { createMediaCenter } from "../dom/mediaCenterElements.js";
 
 export function createSongCard(song) {
 	const songCard = document.createElement('li');
 	songCard.className = "song-list";
 	songCard.innerHTML = `
-		<div id="song-card" class="song-card">
-			<h3>${song.songTitle}</h3>
-			<audio>
-				<source src="../data/audio/${song.songTitle}.mp3">
-			</audio>
-			<div id="controls">
-				<button class="play-button">
-					<svg fill="#000000" height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-					viewBox="0 0 60 60" xml:space="preserve">
-					<g>
-					<path d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30
-						c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15
-						C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z"/>
-					<path d="M30,0C13.458,0,0,13.458,0,30s13.458,30,30,30s30-13.458,30-30S46.542,0,30,0z M30,58C14.561,58,2,45.439,2,30
-						S14.561,2,30,2s28,12.561,28,28S45.439,58,30,58z"/>
-					</g>
-					</svg>
-				</button>
-                <p class="current-time"></p>
-				<input type="range" class="seek-slider" value="0" min="0" max="100">
-                <p class="audio-duration"></p>
-			</div>
-		</div>  
+        <div class="song-card">
+            <div class="song-card-details">
+                <h3>${song.songTitle}</h3>
+                <h4>${song.artistName}</h4>
+            </div>
+            <div id="controls">
+                <button class="play-song-button">
+                    <svg class="play-icon" version="1.1" viewBox="-3 0 28 28" xmlns="http://www.w3.org/2000/svg">
+                        <g transform="translate(-419 -571)" fill="#000">
+                        <path d="m440.42 583.55-18.997-12.243c-1.127-0.607-2.418-0.544-2.418 1.635v24.108c0 1.992 1.385 2.306 2.418 1.635l18.997-12.243c0.782-0.799 0.782-2.093 0-2.892"></path>
+                        </g>
+                    </svg>
+                </button>
+            </div>
+        </div>  
 	`;
 
-    const audio = songCard.querySelector('audio');
-	const slider = songCard.querySelector('.seek-slider');
-	const playButton = songCard.querySelector('.play-button');
-    const audioDurationTag = songCard.querySelector('.audio-duration')
+	const playSong = songCard.querySelector('.play-song-button');
+	const audio = document.querySelector('audio');
 
-	audio.addEventListener('loadedmetadata', function() {
-		slider.max = audio.duration
-	});
+	playSong.addEventListener('click', function() {
 
-	audio.addEventListener('timeupdate', function() {
-		slider.value = currentAudio.currentTime;
-        const currentTimeTag = songCard.querySelector('.current-time')
-        currentTimeTag.textContent = formatTime(currentAudio.currentTime)
+		let volumeLevelTag = document.querySelector('.volume-level')
+		let volume = document.querySelector('.volume-slider')
+		audio.src = "../data/audio/" + song.songTitle + ".mp3"
 
-	});
-
-	playButton.addEventListener('click', function() {
-		handlePlayButtonClick(audio, audioDurationTag);
+		audio.addEventListener('loadedmetadata', function() {
+			createMediaCenter(song);
+			pauseAllAudio();
+			audio.currentTime = 0;
+			resetAllSliders();
+			audio.play();
+			audio.volume = (volume.value / 100)
+			volumeLevelTag = volume.value;
+		});
 	});
 
 	return songCard;
 }
-
-const volume = document.querySelector('.volume-slider')
-
-function formatTime(seconds) {
-    var minutes;
-    minutes = Math.floor(seconds / 60);
-    seconds = Math.floor(seconds % 60);
-    seconds = (seconds >= 10) ? seconds : "0" + seconds;
-    return minutes + ":" + seconds;
-  }
-
-function handlePlayButtonClick(audio, audioDurationTag) {
-	if (audio === currentAudio) {
-		if (audio.paused) {
-			audio.play();
-		} 
-        else {
-			audio.pause();
-		}
-	} 
-    else {
-		pauseAllAudio();
-		currentAudio = audio;
-		audio.currentTime = 0;
-        resetAllSliders();
-		audio.play();
-        audioDurationTag.textContent = formatTime(audio.duration);
-        audio.volume = (volume.value / 100)
-        volumeLevelTag = volume.value;
-	}
-}
-
-document.addEventListener('input', function(event) {
-	const slider = event.target.closest('.seek-slider');
-	if (slider) {
-		const songCard = slider.closest('.song-card');
-		const audio = songCard.querySelector('audio');
-		if (audio) {
-			audio.currentTime = slider.value;
-		}
-	}
-});
-
-volume.addEventListener('input', function(event) { 
-    let volumeLevelTag = document.querySelector('.volume-level')
-    volumeLevelTag.textContent = volume.value + '%';
-    if(currentAudio) {
-        currentAudio.volume = (volume.value / 100)
-    }
-});
 
 function pauseAllAudio() {
 	const audioPlayers = document.querySelectorAll("audio");
--- js/main.js
+++ js/main.js
@@ -4,20 +4,29 @@ import { fetchDatabase } from "../js/api/songs.js";
 import { createSongCard } from "./dom/songElements.js";
 
 const musicList = document.querySelector('.music');
+const volume = document.querySelector('.volume-slider');
+const audio = document.querySelector('audio');
 
 async function loadMusic() {
 	try {
+		let displayedSongs = [];
 		const songs = await fetchDatabase();
 		console.log("Songs fetched:", songs);
-		for (let i = 0; i < 6; i++) {
+		do {
 			let max = songs.length;
 			let min = 0;
 			let num = Math.floor(Math.random() * (max - min) + min);
 			let song = songs[num];
-			let songCard = createSongCard(song);
-			musicList.appendChild(songCard);
+			let index = songs.indexOf(song);
+			let songCard = createSongCard(song); 
+			if (!displayedSongs.includes(index)) {
+				displayedSongs.push(index);
+				musicList.appendChild(songCard);
+			}
 		}
-	} catch (error) {
+		while(musicList.childNodes.length != 6)
+	} 
+	catch (error) {
 		console.error("Error fetching songs:", error);
 	}
 }
@@ -28,7 +37,16 @@ function loadVolumeLevel() {
     volumeLevelTag.textContent = volume.value + '%';
 }
 
+volume.addEventListener('input', function(event) { 
+    let volumeLevelTag = document.querySelector('.volume-level')
+    volumeLevelTag.textContent = volume.value + '%';
+    if(audio) {
+        audio.volume = (volume.value / 100)
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
 	loadMusic();
     loadVolumeLevel();
 });
+
