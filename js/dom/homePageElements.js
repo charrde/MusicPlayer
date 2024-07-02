9ea8a8c506342b4842e48f344fdcc51b50da237e
charrde <charrrde@gmail.com>
2024-07-01 19:07:20 -0600

    Element and styling changes.

---
+++ js/dom/homePageElements.js
@@ -0,0 +1,130 @@
+import { createMediaCenter } from './mediaCenterElements.js';
+
+const mainElement = document.querySelector('main');
+
+export function createTopPlayed() {
+	const topPlayed = document.createElement('div');
+	topPlayed.className = 'hot-now-section';
+	topPlayed.innerHTML = `
+		<section class="center-container">
+			<div class="padding-container">
+				<div class="section-heading">
+					<h2 class="section-heading-text">Hot right now.</h2>
+					<div class="scroll-buttons">
+						<button id="hot-now-scroll-back-button">
+							<p>B</p>
+						</button>
+						<button id="hot-now-scroll-forward-button">
+							<p>F</p>
+						</button>
+					</div>
+				</div>
+				<div class="volume">
+					<h3>Volume</h3>
+					<input type="range" class="volume-slider" value="50" min="0" max="100">
+					<p class="volume-level"></p>
+				</div>
+				<div id="hot-now-songs" class="hot-now-songs">
+					<!-- Song cards will be dynamically inserted here -->
+				</div>
+			</div>
+		</section>
+	`;
+
+	mainElement.appendChild(topPlayed);
+
+	const hotSongsScrollForward = document.getElementById('hot-now-scroll-forward-button');
+	const hotSongsScrollBack = document.getElementById('hot-now-scroll-back-button');
+	const hotSongsScrollingElement = document.getElementById('hot-now-songs');
+
+    hotSongsScrollBack.addEventListener('click', function () {
+        console.log('scroll back');
+        const desiredElement = document.querySelector('.song-card');
+        let scrollDistance = desiredElement.offsetWidth;
+
+        hotSongsScrollingElement.scrollLeft -= scrollDistance;
+        if ((hotSongsScrollForward.disabled = true)) {
+            hotSongsScrollForward.disabled = false;
+        }
+        hotSongsScrollingElement.addEventListener('scrollend', function () {
+            hotSongsScrollBack.disabled = true;
+        });
+    });
+
+    hotSongsScrollForward.addEventListener('click', function () {
+        console.log('scroll forward');
+        const desiredElement = document.querySelector('.song-card');
+        let scrollDistance = desiredElement.offsetWidth;
+
+        hotSongsScrollingElement.scrollLeft += scrollDistance;
+        if ((hotSongsScrollBack.disabled = true)) {
+            hotSongsScrollBack.disabled = false;
+        }
+        hotSongsScrollingElement.addEventListener('scrollend', function () {
+            hotSongsScrollForward.disabled = true;
+        });
+    });
+}
+
+export function createSongCard(song) {
+	const songCard = document.createElement('div');
+	songCard.className = 'song-card';
+	songCard.innerHTML = `
+		<div class="song-card-details">
+			<h3>${song.song_title}</h3>
+			<h4>${song.artist_name}</h4>
+		</div>
+		<div id="controls">
+			<button class="play-song-button">
+				<svg class="play-icon" version="1.1" width="40px" height="40px" viewBox="-3 0 28 28" xmlns="http://www.w3.org/2000/svg">
+					<g transform="translate(-419 -571)" fill="#000">
+						<path d="m440.42 583.55-18.997-12.243c-1.127-0.607-2.418-0.544-2.418 1.635v24.108c0 1.992 1.385 2.306 2.418 1.635l18.997-12.243c0.782-0.799 0.782-2.093 0-2.892"></path>
+					</g>
+				</svg>
+			</button>
+		</div>
+	`;
+
+	const playSong = songCard.querySelector('.play-song-button');
+	const audio = document.querySelector('audio');
+
+	playSong.addEventListener('click', async function () {
+		let volumeLevelTag = document.querySelector('.volume-level');
+		let volume = document.querySelector('.volume-slider');
+
+		try {
+			const decodedFilePath = decodeURIComponent(song.file_path.split('/').pop());
+			const response = await fetch(`https://shmoovin.adaptable.app/presigned-url/${encodeURIComponent(decodedFilePath)}`);
+			const data = await response.json();
+			if (data.url) {
+				audio.src = data.url;
+
+				audio.addEventListener('loadedmetadata', function () {
+					createMediaCenter(song);
+					pauseAllAudio();
+					audio.currentTime = 0;
+					resetAllSliders();
+					audio.play();
+					audio.volume = volume.value / 100;
+					volumeLevelTag = volume.value;
+				});
+			} else {
+				console.error('Pre-signed URL not returned in response:', data);
+			}
+		} catch (error) {
+			console.error('Error fetching pre-signed URL:', error);
+		}
+	});
+
+	return songCard;
+}
+
+function pauseAllAudio() {
+	const audioPlayers = document.querySelectorAll('audio');
+	audioPlayers.forEach(audio => audio.pause());
+}
+
+function resetAllSliders() {
+	const sliders = document.querySelectorAll('.seek-slider');
+	sliders.forEach(slider => (slider.value = 0));
+}
--- js/main.js
+++ js/main.js
@@ -1,74 +1,41 @@
-import { fetchDatabase } from "./api/songs.js";
-import { createSongCard } from "./dom/songElements.js";
-
-const musicList = document.querySelector('.music');
-const volume = document.querySelector('.volume-slider');
-const audio = document.querySelector('audio');
-
-async function headerAuthDisplay() {
-	try {
-		const response = await fetch('/api/auth-check', {
-			credentials: 'include'
-		});
-		const data = await response.json();
-
-		const authDisplayDiv = document.getElementById('header-login-auth-display');
-		authDisplayDiv.innerHTML = '';
-		const pTag = document.createElement('p');
-
-		if (data.authenticated) {
-			const userInfoResponse = await fetch('/api/user-info', {
-				credentials: 'include'
-			});
-			const userInfo = await userInfoResponse.json();
-			pTag.textContent = `User ID: ${userInfo.user.id}`;
-		} else {
-			pTag.textContent = 'Login';
-		}
-
-		authDisplayDiv.appendChild(pTag);
-	} catch (error) {
-		const authDisplayDiv = document.getElementById('header-login-auth-display');
-		authDisplayDiv.innerHTML = '';
-
-		const pTag = document.createElement('p');
-		pTag.textContent = 'Login';
-		authDisplayDiv.appendChild(pTag);
-	}
-}
+import { createTopPlayed, createSongCard } from './homePageElements.js';
+import { fetchDatabase } from './api/songs.js';
 
 async function loadMusic() {
 	try {
 		const response = await fetchDatabase();
 		const songs = response.songs;
-		musicList.innerHTML = ``;
+		const hotNowSongsContainer = document.getElementById('hot-now-songs');
+		hotNowSongsContainer.innerHTML = '';
 
 		songs.forEach(song => {
 			const songCard = createSongCard(song);
-			musicList.appendChild(songCard);
+			hotNowSongsContainer.appendChild(songCard);
 		});
 		loadVolumeLevel();
-	} 
-	catch (error) {
-		console.error("Error fetching songs:", error);
+	} catch (error) {
+		console.error('Error fetching songs:', error);
 	}
 }
 
 function loadVolumeLevel() {
-    const volume = document.querySelector('.volume-slider')
-    let volumeLevelTag = document.querySelector('.volume-level')
-    volumeLevelTag.textContent = volume.value + '%';
+	const volume = document.querySelector('.volume-slider');
+	let volumeLevelTag = document.querySelector('.volume-level');
+	volumeLevelTag.textContent = volume.value + '%';
 }
 
-volume.addEventListener('input', function(event) { 
-    let volumeLevelTag = document.querySelector('.volume-level')
-    volumeLevelTag.textContent = volume.value + '%';
-    if(audio) {
-        audio.volume = (volume.value / 100)
-    }
+const volume = document.querySelector('.volume-slider');
+const audio = document.querySelector('audio');
+
+volume.addEventListener('input', function (event) {
+	let volumeLevelTag = document.querySelector('.volume-level');
+	volumeLevelTag.textContent = volume.value + '%';
+	if (audio) {
+		audio.volume = volume.value / 100;
+	}
 });
 
-document.addEventListener('DOMContentLoaded', async function() {
-		headerAuthDisplay();
-		await loadMusic();
+document.addEventListener('DOMContentLoaded', async function () {
+	createTopPlayed();
+	await loadMusic();
 });
